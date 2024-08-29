654ab70b2f9b56b5e8c4d9c06e9560eab0fa1086
samuel daniels <s.daniels@myzeepay.com>
2024-08-29 23:58:58 +0100

    added backend theme

--- src/controllers/dashboard/DashboardController.js
+++ src/controllers/dashboard/DashboardController.js
@@ -5,12 +5,32 @@ const Admin = require('../../models/admin.model');
 const Schedule = require('../../models/schedule.model');
 const CompletedJob = require("../../models/completed-job.model");
 
-async function getIndex1(req, res) {
-
-}
-
 
 async function getIndex(req, res) {
+    let totalUsers, systemUsers, totalSchedules, completePickups;
+    try {
+        totalUsers = await User.countDocuments({ role: 'Subscriber' });
+        systemUsers = await Admin.countDocuments({ });
+        totalSchedules = await Schedule.countDocuments({});
+        completePickups = await CompletedJob.countDocuments({});
+        
+    } catch (error) {
+        console.error(error);
+    }
+    return res.render('backend/index', {
+        pageTitle: 'Dashboard',
+        path: '/admin/index',
+        errors: false,
+        errorMessage: false,
+        csrfToken: req.csrfToken(),
+        totalUsers: totalUsers ? totalUsers : 0,
+        systemUsers: systemUsers ? systemUsers : 0,
+        totalSchedules: totalSchedules ? totalSchedules : 0,
+        completePickups: completePickups ? completePickups : 0,
+    });
+}
+
+async function getIndex1(req, res) {
     let totalUsers, systemUsers, totalSchedules, completePickups;
     try {
         totalUsers = await User.countDocuments({ role: 'Subscriber' });
@@ -37,5 +57,5 @@ async function getIndex(req, res) {
 
 module.exports = {
     getIndex,
-    getIndex1
+    getIndex1,
 };
