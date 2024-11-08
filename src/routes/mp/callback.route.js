aedf9226c2aa9fcc16421082ae81c98cc3ee74ee
samuel daniels <sd24434@essex.ac.uk>
2024-11-08 13:06:08 +0000

    tem

--- src/routes/mp/callback.route.js
+++ src/routes/mp/callback.route.js
@@ -6,43 +6,38 @@ const router = express.Router();
 
 const callBackController = require("../../controllers/v1/CallbackController");
 
-const isWhitelisted = require("../../Middleware/is-whitelisted-IP");
 
 // post wallet topup callback
 router.post(
 	"/wallet-topup-callback",
-	isWhitelisted,
 	callBackController.postWalletTopupCallback
 );
 // post transaction callback
 router.post(
 	"/transaction-callback",
-	isWhitelisted,
 	callBackController.postTransactionCallback
 );
 // post hubtel airtime callback
 router.post(
 	"/hubtel-airtime-callback",
-	isWhitelisted,
 	callBackController.postHubtelAirtimeTopup
 );
 // post hubtel payment callback
 router.post(
 	"/hubtel-payment-callback",
-	isWhitelisted,
 	callBackController.postHubtelPaymentCallback
 );
 // post utility services callback
 router.post(
 	"/hubtel-utility-services-callback",
-	isWhitelisted,
 	callBackController.postHubtelUtilityCallbackServices
 );
 // post balance transfer callback
 router.post(
 	"/hubtel-balance-transfer-callback",
-	isWhitelisted,
 	callBackController.postHubtelTransferBalanceCallback
 );
+
+
 
 module.exports = router;
