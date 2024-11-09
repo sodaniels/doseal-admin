5c8abde3179225357e704b4ed51b7854775cfbec
samuel daniels <sd24434@essex.ac.uk>
2024-11-09 15:06:05 +0000

    added middlware

--- src/Middleware/is-superUser.js
+++ src/Middleware/is-superUser.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     if(req.session.user.role !== 'Super Admin'){
-        return res.redirect('/login')
+        return res.redirect('/auth/login?redirectUrl=https://doseal.org/login/redirect')
     }
     next();
 }
